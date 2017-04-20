780fb4fdf0ca7d53826f44c1988afbc4cd04e728
Satoshi UENO <satueno@yahoo-corp.jp>
2017-04-20 16:13:33 +0900

    Output message to stdout.

--- src/main/java/com/example/HelloApplication.java
+++ src/main/java/com/example/HelloApplication.java
@@ -1,12 +1,17 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloApplication {
-
+public class HelloApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
 	}
 }
