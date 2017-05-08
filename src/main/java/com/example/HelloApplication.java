92d48c12790064661a6984aa0835098fed37e5bd
Satoshi UENO <satueno@yahoo-corp.jp>
2017-05-08 15:45:33 +0900

    出力する文字列の解決にDIを活用

--- src/main/java/com/example/HelloApplication.java
+++ src/main/java/com/example/HelloApplication.java
@@ -1,17 +1,21 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloApplication implements CommandLineRunner {
+	@Autowired
+	private GreetingService greetingService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World!");
+		System.out.println(greetingService.greet());
 	}
 }
